e7b7336f53ce019a1ef6ff168d0f6539719daef6
Dr Nehru Healthcare System <drnehru@homahealthcare.com>
2025-10-11 08:51:43 +0530

    Initial commit: Homa Healthcare Payment System

---
+++ server.js
@@ -0,0 +1,36 @@
+﻿const express = require('express');
+const twilio = require('twilio');
+const TelegramBot = require('node-telegram-bot-api');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_ADMIN_CHAT_ID = process.env.TELEGRAM_ADMIN_CHAT_ID;
+
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
+const AIRTABLE_TABLE_ID = process.env.AIRTABLE_TABLE_ID;
+
+const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+const telegramBot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const paymentSessions = new Map();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:' + PORT);
+});
